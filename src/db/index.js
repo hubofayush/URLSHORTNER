0acf44bcafca740fd2a60bd426e5edb95191e22d
Ayush <ayushempire03@gmail.com>
2024-12-25 13:11:06 +0530

    established connection bet mongdb

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+const connectDB = async () => {
+    console.log(process.env.MONGOURL);
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGOURL}${DB_NAME}`
+        );
+        console.log(
+            `\nMongoDB connected to DB HOST:${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log("Mongodb conncection Error: ", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+dotenv.config({
+    path: "./.env",
 });
+
+connectDB()
+    .then(() => {
+        app.listen(4000, () => {
+            console.log("running on port 4000");
+        });
+    })
+    .catch((error) => {
+        console.log("mongodb connection error", error);
+    });
